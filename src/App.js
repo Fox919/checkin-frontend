80124e2604d074529e1e9ac8bff0834f5314b94f
Fox919 <729641995q@gmail.com>
2026-04-24 23:07:54 -0700

    add nav

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Register from './components/Register'; // 假設你的 Register 在這裡
-import AdminList from './components/AdminList'; // 匯入你的管理頁面
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Register from './components/Register';
+import AdminList from './components/AdminList';
 
 function App() {
   return (
     <BrowserRouter>
+      {/* 這部分放在 Routes 外面，所以它會一直存在於所有頁面 */}
+      <nav style={{ padding: '15px', borderBottom: '1px solid #ccc', textAlign: 'center' }}>
+  <Link to="/">
+    <button>前往登記</button>
+  </Link>
+  <Link to="/admin" style={{ marginLeft: '10px' }}>
+    <button>前往管理員後台</button>
+  </Link>
+</nav>
+
+      {/* 這是頁面切換區 */}
       <Routes>
-        {/* 使用者登記頁面 */}
         <Route path="/" element={<Register />} />
-        
-        {/* 管理員簽到頁面，網址是 /admin */}
         <Route path="/admin" element={<AdminList />} />
       </Routes>
     </BrowserRouter>
